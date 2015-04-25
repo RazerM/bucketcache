92cc6c6a807fe352b9ee4d51c68372ecf7e384da
Frazer McLean <frazer@frazermclean.co.uk>
2015-04-25 02:18:07 +0200

    Make BackendConfig repr match arg order of __init__

--- bucketcache/config.py
+++ bucketcache/config.py
@@ -1,7 +1,12 @@
 from __future__ import absolute_import, division, print_function
 
+import inspect
+from collections import OrderedDict
+
 import six
 from represent import PrettyRepresentationHelper, RepresentationHelper
+
+from .utilities import _fullargspec_from_argspec
 
 __all__ = (
     'PickleConfig',
@@ -11,13 +16,36 @@ __all__ = (
 
 
 class BackendConfig(object):
-    def __init__(self, **kwargs):
-        for name, value in six.iteritems(kwargs):
-            setattr(self, name, value)
+    def __init__(self, ordered_args, allow_unordered=False):
+        if not allow_unordered:
+            try:
+                argspec = inspect.getfullargspec(self.__init__)
+            except AttributeError:
+                argspec = inspect.getargspec(self.__init__)
+                argspec = _fullargspec_from_argspec(argspec)
+
+            if list(ordered_args.keys()) != argspec.args[1:]:
+                raise TypeError(
+                    'By default, subclasses of BackendConfig must be passed an'
+                    ' ordered dictionary of arguments that matches the order '
+                    'in {}.__init__(). Otherise, pass allow_unordered=True to '
+                    'BackendConfig.__init__()'.format(self.__class__.__name__))
+
+        self._attrs = ordered_args
+
+    def __getattr__(self, name):
+        try:
+            return self._attrs[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        if name == '_attrs':
+            return super(BackendConfig, self).__setattr__(name, value)
+        self._attrs[name] = value
 
     def asdict(self):
-        return {k: v for k, v in six.iteritems(self.__dict__)
-                if not k.startswith('_')}
+        return self._attrs
 
     def _repr_helper(self, r):
         for name in self.asdict():
@@ -37,9 +65,13 @@ class PickleConfig(BackendConfig):
     def __init__(self, protocol=2, fix_imports=True,
                  encoding='ASCII', errors='strict'):
 
-        super(PickleConfig, self).__init__(
-            protocol=protocol, fix_imports=fix_imports,
-            encoding=encoding, errors=errors)
+        okwargs = OrderedDict([
+            ('protocol', protocol),
+            ('fix_imports', fix_imports),
+            ('encoding', encoding),
+            ('errors', errors)])
+
+        super(PickleConfig, self).__init__(okwargs)
 
 
 class JSONConfig(BackendConfig):
@@ -49,22 +81,24 @@ class JSONConfig(BackendConfig):
                  decoder_cls=None, object_hook=None, parse_float=None,
                  parse_int=None, parse_constant=None, object_pairs_hook=None):
 
-        super(JSONConfig, self).__init__(
-            skipkeys=skipkeys,
-            ensure_ascii=ensure_ascii,
-            check_circular=check_circular,
-            allow_nan=allow_nan,
-            encoder_cls=encoder_cls,
-            indent=indent,
-            separators=separators,
-            default=default,
-            sort_keys=sort_keys,
-            decoder_cls=decoder_cls,
-            object_hook=object_hook,
-            parse_float=parse_float,
-            parse_int=parse_int,
-            parse_constant=parse_constant,
-            object_pairs_hook=object_pairs_hook)
+        okwargs = OrderedDict([
+            ('skipkeys', skipkeys),
+            ('ensure_ascii', ensure_ascii),
+            ('check_circular', check_circular),
+            ('allow_nan', allow_nan),
+            ('encoder_cls', encoder_cls),
+            ('indent', indent),
+            ('separators', separators),
+            ('default', default),
+            ('sort_keys', sort_keys),
+            ('decoder_cls', decoder_cls),
+            ('object_hook', object_hook),
+            ('parse_float', parse_float),
+            ('parse_int', parse_int),
+            ('parse_constant', parse_constant),
+            ('object_pairs_hook', object_pairs_hook)])
+
+        super(JSONConfig, self).__init__(okwargs)
 
 
 class MessagePackConfig(BackendConfig):
@@ -73,15 +107,17 @@ class MessagePackConfig(BackendConfig):
                  use_bin_type=1, object_hook=None, list_hook=None, use_list=1,
                  unpack_encoding='utf-8', object_pairs_hook=None):
 
-        super(MessagePackConfig, self).__init__(
-            default=default,
-            pack_encoding=pack_encoding,
-            unicode_errors=unicode_errors,
-            use_single_float=use_single_float,
-            autoreset=autoreset,
-            use_bin_type=use_bin_type,
-            object_hook=object_hook,
-            list_hook=list_hook,
-            use_list=use_list,
-            unpack_encoding=unpack_encoding,
-            object_pairs_hook=object_pairs_hook)
+        okwargs = OrderedDict([
+            ('default', default),
+            ('pack_encoding', pack_encoding),
+            ('unicode_errors', unicode_errors),
+            ('use_single_float', use_single_float),
+            ('autoreset', autoreset),
+            ('use_bin_type', use_bin_type),
+            ('object_hook', object_hook),
+            ('list_hook', list_hook),
+            ('use_list', use_list),
+            ('unpack_encoding', unpack_encoding),
+            ('object_pairs_hook', object_pairs_hook)])
+
+        super(MessagePackConfig, self).__init__(okwargs)
