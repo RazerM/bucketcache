f10a437335c751f95e11ff51f5a345bc0513d538
Frazer McLean <frazer@frazermclean.co.uk>
2015-10-02 04:42:35 +0200

    Fix documentation error

--- bucketcache/exceptions.py
+++ bucketcache/exceptions.py
@@ -10,19 +10,17 @@ __all__ = (
 
 class KeyInvalidError(Exception):
     """Raised internally when a key cannot be loaded."""
-    pass
 
 
 class KeyFileNotFoundError(KeyInvalidError):
     """Raised when file for key doesn't exist."""
-    pass
 
 
 class KeyExpirationError(KeyInvalidError):
     """Raised when key has expired."""
-    pass
 
 
 class BackendLoadError(Exception):
-    """Raised when Backend.from_file cannot load an object."""
-    pass
+    """Raised when :py:meth:`bucketcache.backends.Backend.from_file` cannot
+    load an object.
+    """
